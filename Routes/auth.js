a4f485d0903625849bdee5b8056edac370830f25
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-03-07 23:43:27 +0530

    added Session based Authentication,API rate limiting and configured mongo session store

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,10 +3,9 @@ import express from "express";
 const router = express.Router();
 import { login, logout, signup } from "../controllers/auth.js";
 import { logRequestDetails, requestRateLimiter } from "../middlewares/commonWare.js";
-import { isAuthenticated } from "../middlewares/auth.js";
 
 router.post("/login", requestRateLimiter, logRequestDetails, login);
-router.get("/logout", isAuthenticated, logout);
+router.get("/logout", logout);
 router.post("/signup", signup);
 
 export default router;
